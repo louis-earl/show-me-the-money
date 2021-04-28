33d22a2e69f1a210a20c7aacf766a07534f150f4
KillickNZ <sebastian.g.chapman@gmail.com>
2021-04-29 08:42:28 +1200

    created time display function

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -2,23 +2,37 @@ import React, { useState, useEffect } from 'react'
 import {connect} from 'react-redux'
 
 const Meeting = () => {
-  const [ runningTime, setRunningTime ] = useState(0) // time in seconds
+  const [ runningTime, setRunningTime ] = useState(5652) // time in seconds
   const [ startStop, setStartStop ] = useState(false) // use Start and Stop as values
   
   const timer = () => {
-
+    let time = setInterval(runningTime, 1000)
   }
+
+  const startStopFuc = (e) => {
+    {(startStop == true) && console.log("This will send meeting data to db")}
+    setStartStop(!startStop)
+  }
+
+  const displayTime = () => {
+    {/*  DISPLAY: hours | min | sec */}
+    let hours = Math.floor(runningTime / 60 /60)
+    let min = Math.floor(runningTime / 60)
+    let sec = runningTime % 60
+    return <h3>{hours} | {min} | {sec}</h3>
+  }
+
   return <div className="container">
     <h2 className="title is-2">Meeting: {/* DISPLAY MEETING ID */}</h2> 
     <ul>
       {/*  MAP THROUGH EMPLOYEES DISPLAY IN li/button*/}
     </ul>
     <div>
-      <div className="timer">{/*  DISPLAY: hours | min | sec */}</div>
+      <div className="timer">{displayTime()} </div>
       <div className="running cost">{/*  DISPLAY: running cost */}</div>
     </div>
     <div>
-      <button onClick={() => setStartStop(!startStop)}>{startStop ? <p>Stop</p> : <p>Start</p>}</button>
+      <button onClick={(e) => startStopFuc()}>{startStop ? <p>Stop</p> : <p>Start</p>}</button>
     </div>
   </div>
 }
