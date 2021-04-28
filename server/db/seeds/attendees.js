b7b246027effc045a49e2ba3b84193bbe4993b60
Alex Bartley Nees <alexbartleynees@gmail.com>
2021-04-28 18:46:16 +1200

    Add seeds file for attendees file

---
+++ server/db/seeds/attendees.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('attendees').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('attendees').insert([
+        {user_id: 1, meeting_id: 1},
+        {user_id: 1, meeting_id: 2},
+        {user_id: 1, meeting_id: 3}
+      ]);
+    });
+};
