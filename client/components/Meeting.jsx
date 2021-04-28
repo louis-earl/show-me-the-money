7ac6a677c25103058dae3e8ed10fb63936e1ce9d
KillickNZ <sebastian.g.chapman@gmail.com>
2021-04-29 08:22:39 +1200

    got start stop button working

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {connect} from 'react-redux'
 
-function Meeting () {
+const Meeting = () => {
+  const [ runningTime, setRunningTime ] = useState(0) // time in seconds
+  const [ startStop, setStartStop ] = useState(false) // use Start and Stop as values
+  
+  const timer = () => {
+
+  }
   return <div className="container">
-    <h2 className="title is-2">Start Meeting</h2>
+    <h2 className="title is-2">Meeting: {/* DISPLAY MEETING ID */}</h2> 
+    <ul>
+      {/*  MAP THROUGH EMPLOYEES DISPLAY IN li/button*/}
+    </ul>
+    <div>
+      <div className="timer">{/*  DISPLAY: hours | min | sec */}</div>
+      <div className="running cost">{/*  DISPLAY: running cost */}</div>
+    </div>
+    <div>
+      <button onClick={() => setStartStop(!startStop)}>{startStop ? <p>Stop</p> : <p>Start</p>}</button>
+    </div>
   </div>
 }
 
