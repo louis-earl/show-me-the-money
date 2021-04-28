6cdc07835e57eb4cacd31884cea846ff34807775
Alex Bartley Nees <alexbartleynees@gmail.com>
2021-04-28 18:34:53 +1200

    Add table name to seeds file

--- server/db/seeds/meetings.js
+++ server/db/seeds/meetings.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
   return knex('meetings').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('meetings').insert([
         {id: 1, meeting_name: 'Ninja Session', attendees: 0, cost: 0 },
         {id: 2, meeting_name: 'Angry Penguins', attendees: 0, cost: 0 },
         {id: 3, meeting_name: 'All the Money', attendees: 0, cost: 0 }
