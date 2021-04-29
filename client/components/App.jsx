d6c13870ac449df64b261ed018cf7f967b7eb401
Dianne <77766667+dianne-tennent@users.noreply.github.com>
2021-04-30 11:16:10 +1200

    nav

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,53 +1,58 @@
-import React, {useEffect} from 'react'
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
-import {connect} from 'react-redux'
+import React, { useEffect } from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import Login from './Login'
 import Register from './Register'
 import Nav from './Nav'
 import Meeting from './Meeting'
-import History from './History'
+import Dashboard from './Dashboard'
 
-import {checkAuth} from '../actions/auth'
+import { checkAuth } from '../actions/auth'
 import { fetchUsers } from '../actions/users'
 
-function App ({auth, dispatch}) {
-  
+function App({ auth, dispatch }) {
+
   useEffect(() => {
     const confirmSuccess = () => { }
     dispatch(checkAuth(confirmSuccess))
-    
+
   }, [])
 
-    return (
-      <Router>
-        <div className="container has-text-centered">
+  return (
+    <Router>
+      <div className="container has-text-centered">
 
-          <div className="hero is-small is-primary">
-            <div className="hero-body has-text-centered">
-              <Link to='/' className="">
-                <h1 className="title is-1">$how Me The Money</h1>
-              </Link>
-              <Route path="/" component={Nav} />
-            </div>
+        <div className="hero is-small is-primary">
+          <div className="hero-body has-text-centered">
+            <Link to='/' className="">
+              <h1 className="title is-1">$how Me The Money</h1>
+            </Link>
+            <Route path="/" component={Nav} />
           </div>
-
-          <div className=''>
-            {!auth.isAuthenticated &&
-              <Route exact path="/" component={Login} />
-            }
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/meeting" component={Meeting} />
-            <Route path="/history" component={History} />
-          </div>
-
         </div>
-      </Router>
-    )
+
+        <div className=''>
+          {auth.isAuthenticated ?
+            <Route exact path="/" component={Dashboard} />
+            :
+            <Route exact path="/" component={Login} />
+
+          }
+          
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/meeting" component={Meeting} />
+          <Route exact path="/dashboard" component={Dashboard} />
+      
+        </div>
+
+      </div>
+    </Router>
+  )
 }
 
-const mapStateToProps = ({auth}) => {
+const mapStateToProps = ({ auth }) => {
   return {
     auth
   }
