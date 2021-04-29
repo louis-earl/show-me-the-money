0a69ce69298df54c40f408c029695b579db8ecdf
Dianne <77766667+dianne-tennent@users.noreply.github.com>
2021-04-29 14:02:55 +1200

    API save meeting routes

---
+++ server/routes/meetings.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+const db = require('../db/meetings')
+
+router.post('/', (req, res) => {
+  db.saveMeeting(meeting)
+  .then((res) => {
+    res.redirect('/meeting')
+  })
+  .catch(err => {
+    res.status(500).send(err.message)
+  })
+})
+
