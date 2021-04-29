fbbf279e1c60e7e800f7bf3442f32320e5170d61
Louis <louis.earl.14@gmail.com>
2021-04-29 23:51:15 +1200

    Display meeting history for the logged in user

--- client/components/PastMeetingSummary.jsx
+++ client/components/PastMeetingSummary.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 import { receiveMeetings } from '../actions/meetings'
 
 function PastMeetingSummary(props) {
-    const {meetingName, cost} = props.meeting
+    const { id, meeting_name, cost, attendees } = props.meeting
 
-  return <div className="container">
-    <h3 className="title is-3">{meetingName}</h3>
-    <p>Cost: {cost}</p>
-  </div>
+    return (
+        <Link to={"/past-meeting/" + id}>
+            <div className="card">
+                <h3 className="title is-3">{meeting_name}</h3>
+                <p>Cost: {cost}</p>
+                <p>Attendees: {attendees}</p>
+            </div>
+        </Link>
+    )
 }
 
 
