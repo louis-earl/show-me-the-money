10ea50217691bd25a54db0191fd38d3f06c5e74b
Louis <louis.earl.14@gmail.com>
2021-06-26 00:14:57 +1200

    refactors CurrentMeeting

--- client/components/meeting/CurrentMeeting.jsx
+++ client/components/meeting/CurrentMeeting.jsx
@@ -1,49 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react"
+import { connect } from "react-redux"
 
-import Ticker from "./Ticker";
-import AddAttendees from "./AddAttendees";
+import Ticker from "./Ticker"
+import AddAttendees from "./AddAttendees"
 
-import { saveMeeting } from "../../apis/meetings";
-import { saveAttendees } from "../../apis/attendees"
-import { fetchUsers } from "../../actions/users";
-import {
-  startMeeting,
-  endMeeting,
-  resetMeeting,
-  addMeetingName,
-} from "../../actions/currentMeeting";
-import { addMeeting } from "../../actions/meetings";
-
-import { formatAttendees } from "../../utils/meeting"
-
+import { endMeeting } from "../../actions/currentMeeting"
 
 const CurrentMeeting = (props) => {
 
   const currentMeeting = props.currentMeeting
   const [usersInMeeting, setUsersInMeeting] = useState([])
 
-  const showSave = !currentMeeting.meetingInProgress && currentMeeting.end_time
-
   const handleClick = () => {
-    props.dispatch(endMeeting());
+    props.dispatch(endMeeting())
   }
-
-  const saveMeeting = () => {
-    props.dispatch(addMeeting(currentMeeting))
-      .then((res) => (saveAttendees(formatAttendees(res.id, currentMeeting))));
-    props.dispatch(resetMeeting());
-    props.dispatch(fetchUsers());
-    setLocalMeetingName("")
-  }
-
-  const refresh = () => {
-    props.dispatch(resetMeeting());
-    setUsersInMeeting([])
-    props.dispatch(fetchUsers());
-    setLocalMeetingName("")
-  }
-
 
   return (
     <>
@@ -51,34 +21,19 @@ const CurrentMeeting = (props) => {
         <h2>{currentMeeting.name}</h2>
       </div>
 
-      <div className="meeting-page">
+      <Ticker />
 
-        <div className="meeting-controls">
+      <AddAttendees usersInMeeting={usersInMeeting} setUsersInMeeting={setUsersInMeeting} />
 
-
-          <Ticker />
-
-            <>
-              <AddAttendees usersInMeeting={usersInMeeting} setUsersInMeeting={setUsersInMeeting} />
-
-              <div className="columns">
-
-                <button className="button--warning" onClick={(e) => handleClick()}>
-                  End Meeting
-                </button>
-
-              </div>
-
-            </>
- 
-
-        </div>
-
+      <div className="columns">
+        <button className="button--warning" onClick={(e) => handleClick()}>
+          End Meeting
+        </button>
       </div>
 
     </>
-  );
-};
+  )
+}
 
 function mapStateToProps(globalState) {
   return {
@@ -86,4 +41,4 @@ function mapStateToProps(globalState) {
     currentUsers: globalState.currentUsers,
   }
 }
-export default connect(mapStateToProps)(CurrentMeeting);
+export default connect(mapStateToProps)(CurrentMeeting)
