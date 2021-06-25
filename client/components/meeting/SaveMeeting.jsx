6d4b4cf3592d4fb9669badb37e2636c3b526b771
Louis <louis.earl.14@gmail.com>
2021-06-25 23:56:48 +1200

    refactors SaveMeeting

--- client/components/meeting/SaveMeeting.jsx
+++ client/components/meeting/SaveMeeting.jsx
@@ -1,80 +1,31 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { connect } from "react-redux"
 
-import { saveMeeting } from "../../apis/meetings"
 import { saveAttendees } from "../../apis/attendees"
 import { fetchUsers } from "../../actions/users"
-import {
-  startMeeting,
-  endMeeting,
-  resetMeeting,
-  addMeetingName,
-} from "../../actions/currentMeeting"
+import { resetMeeting } from "../../actions/currentMeeting"
 import { addMeeting } from "../../actions/meetings"
-
 import { formatAttendees } from "../../utils/meeting"
 
-import CurrentMeeting from "./CurrentMeeting"
-import NewMeeting from "./NewMeeting"
-
-
-const Meeting = (props) => {
-  const [usersInMeeting, setUsersInMeeting] = useState([])
-  const [localMeetingName, setLocalMeetingName] = useState("")
-  const [isNameError, setIsNameError] = useState(false)
-
-
-  useEffect(() => {
-    props.dispatch(fetchUsers())
-  }, [])
-
-  const meetingInProgress = props.currentMeeting.meetingInProgress
-  const showSave = !props.currentMeeting.meetingInProgress && props.currentMeeting.end_time
-
-  const handleClick = () => {
-    if (localMeetingName !== "") {
-      setIsNameError(false)
-
-      if (!meetingInProgress) {
-        props.dispatch(startMeeting(props.currentUsers, localMeetingName));
-      }
-      else {
-        props.dispatch(endMeeting());
-      }
-
-    }
-    else {
-      setIsNameError(true)
-    }
-  }
+const SaveMeeting = (props) => {
 
   const saveMeeting = () => {
     props.dispatch(addMeeting(props.currentMeeting))
       .then((res) => (saveAttendees(formatAttendees(res.id, props.currentMeeting))));
-    props.dispatch(resetMeeting());
-    props.dispatch(fetchUsers());
-    setLocalMeetingName("")
+      refresh()
   }
 
   const refresh = () => {
-    props.dispatch(resetMeeting());
-    setUsersInMeeting([])
-    props.dispatch(fetchUsers());
-    setLocalMeetingName("")
-  }
-
-  const handleChange = (e) => {
-    setLocalMeetingName(e.target.value);
+    props.dispatch(resetMeeting())
+    props.dispatch(fetchUsers())
   }
 
   return (
-
     <div>
       <h3>Save {props.currentMeeting.meeting_name}?</h3>
       <button onClick={saveMeeting}>Yeah!</button>
       <button className="button--warning" onClick={refresh}>Nah</button>
     </div>
-
   )
 }
 
@@ -83,4 +34,4 @@ function mapStateToProps(globalState) {
     currentMeeting: globalState.currentMeeting,
   };
 }
-export default connect(mapStateToProps)(Meeting);
+export default connect(mapStateToProps)(SaveMeeting);
