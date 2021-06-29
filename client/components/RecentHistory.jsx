93c176a7b005b52447d6307a38d88c9e6100416a
Louis <louis.earl.14@gmail.com>
2021-06-29 20:29:48 +1200

    restyle buttons to recent-history grid

--- client/components/RecentHistory.jsx
+++ client/components/RecentHistory.jsx
@@ -18,22 +18,26 @@ function RecentHistory(props) {
     </div>
     {
       props.meetingsHistory.length > 0 ?
-        <div className="history__grid"> {
+        <div className="recent-history__grid"> {
 
-          props.meetingsHistory.map((meeting) => {
-            return <PastMeetingSummary key={meeting.id} meeting={meeting} />
+          props.meetingsHistory.map((meeting, index) => {
+            if (index < 5)
+              return <PastMeetingSummary key={meeting.id} meeting={meeting} />
           })}
 
-          <Link to="/meeting">
-            <div className="card__outer">
-              <div className="card">
-                <h3>Add new meeting</h3>
-                <div className="add__wrapper">
-                <img className="add" src="./images/add.png" />
-                </div>
+          <div className="recent-history__buttons">
+            <Link to="/meeting">
+                <div className="card">
+                  <h3>Add New Meeting</h3>
               </div>
-            </div>
-          </Link>
+            </Link>
+  
+            <Link to="/history">
+                <div className="card">
+                  <h3>See All Meetings</h3>
+                </div>
+            </Link>
+          </div>
 
         </div>
 
