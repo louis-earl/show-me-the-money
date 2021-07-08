d84bd1de4930a09996aafd6883274b57f7ae9313
Louis <louis.earl.14@gmail.com>
2021-07-08 19:39:30 +1200

    user wage parse to float from db

--- server/db/users.js
+++ server/db/users.js
@@ -20,12 +20,20 @@ function userExists (username, db = connection) {
 function getUserByUsername (username, db = connection) {
   return db('users')
     .where('username', username)
+    .then(u => {
+      return {...u, hourly_wage: parseFloat(u.hourly_wage)}
+    })
     .first()
 }
 
 function getAllUsers (db = connection) {
   return db('users')
   .select()
+  .then(users => {
+    return users.map(u => {
+        return { ...u, hourly_wage: parseFloat(u.hourly_wage) }
+    })
+})
   .catch((err) => {
     console.log(err.message)
   })
