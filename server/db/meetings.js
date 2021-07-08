17f6c376bd3870552135b50eeec67c2ad7eda3d9
Louis <louis.earl.14@gmail.com>
2021-07-08 18:47:06 +1200

    fixes return for db getMeetings

--- server/db/meetings.js
+++ server/db/meetings.js
@@ -1,61 +1,60 @@
 const connection = require('./connection')
 
-function saveMeeting (obj, db = connection) {
+function saveMeeting(obj, db = connection) {
     return db('meetings')
-    .insert(obj, 'id')
-    .then(meetingID => getMeetingByMeetingID(meetingID))
-    .catch((err) => {
-        console.log(err.message)
-      })
-}
-
-function getUsersMeetingHistory (id, db = connection) {
-    return db('meetings')
-    .join('attendees', 'meetings.id', 'attendees.meeting_id')
-    .where('user_id', id)
-    .select()
-    .then(meetings => {
-        meetings.map(m => {
-            return {...m, cost: parseFloat(m.cost)}
+        .insert(obj, 'id')
+        .then(meetingID => getMeetingByMeetingID(meetingID))
+        .catch((err) => {
+            console.log(err.message)
         })
-        return meetings
-    })
-    .catch((err) => {
-        console.log(err.message)
-      })
-
 }
 
-function getMeetingAttendees (id, db = connection) {
-    return db('users')
-    .join('attendees', 'users.id', 'attendees.user_id')
-    .where('meeting_id', id)
-    .select()
-    .catch((err) => {
-        console.log(err.message)
-      })
-}
-
-function getMeetingByMeetingID (meetingID, db = connection) {
+function getUsersMeetingHistory(id, db = connection) {
     return db('meetings')
-    .where('id', meetingID)
-    .select()
-    .first()
-    .then(m => {
-        console.log("get meeting by meetingId", m)
-        return m
-    })
-    .catch((err) => {
-        console.log(err.message)
-      })
+        .join('attendees', 'meetings.id', 'attendees.meeting_id')
+        .where('user_id', id)
+        .select()
+        .then(meetings => {
+            return meetings.map(m => {
+                return { ...m, cost: parseFloat(m.cost) }
+            })
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+
 }
 
-function saveAttendees (arr, db = connection) {
+function getMeetingAttendees(id, db = connection) {
+    return db('users')
+        .join('attendees', 'users.id', 'attendees.user_id')
+        .where('meeting_id', id)
+        .select()
+        .catch((err) => {
+            console.log(err.message)
+        })
+}
+
+function getMeetingByMeetingID(meetingID, db = connection) {
+    return db('meetings')
+        .where('id', meetingID)
+        .select()
+        .first()
+        .then(m => {
+            console.log("get meeting by meetingId", m)
+            return m
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+}
+
+function saveAttendees(arr, db = connection) {
     return db('attendees')
-    .insert(arr)
-    .catch((err) => {
-        console.log(err.message)
-      })
+        .insert(arr)
+        .catch((err) => {
+            console.log(err.message)
+        })
 }
 
 
